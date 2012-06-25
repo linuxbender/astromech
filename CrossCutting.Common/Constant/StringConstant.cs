62f1223ef16c74b2b400d466cc1f4e9398b84bc9
daniel glenn <linuxbender@gmail.com>
2012-06-25 11:00:35 +0200

    init basic mvc project

---
+++ CrossCutting.Common/Constant/StringConstant.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CrossCutting.Common.Constant
+{
+    public static class StringConstant
+    {
+
+        public const string EMPTY = "";
+
+        #region HtmlExtensions
+
+        public const string APP_VD_TITEL = "AppTitle";        
+        public const string APP_NAME = "Astromech";
+        public const string APP_NAME_EX = "Astromech | {0}";
+       
+        public const string GRAVATAR_SIZE = "80";
+        public const string GRAVATAR_LINK = "https://secure.gravatar.com/avatar/{0}?s={1}";
+        public const string GRAVATAR_MD5_RE = "-";
+
+        public const string HTML_TAG_IMG = "img";
+        public const string HTML_ATTRI_IMG_SRC = "src";
+        public const string HTML_ATTRI_IMG_ALT = "alt";
+               
+        #endregion
+    }
+}
---
+++ Presentation.Web.Utils/Extension/HtmlExtensions.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web.Mvc;
+using System.Web.Routing;
+using CrossCutting.Common.Constant;
+
+namespace Presentation.Web.Utils.Extension
+{
+    public static class HtmlExtensions
+    {
+        public static MvcHtmlString AppTitle(this HtmlHelper html)
+        {
+            if (html.ViewData[StringConstant.APP_VD_TITEL] != null)
+            {
+                if (!string.IsNullOrWhiteSpace(html.ViewData[StringConstant.APP_VD_TITEL].ToString()))
+                {
+                    return new MvcHtmlString(
+                        string.Format(StringConstant.APP_NAME_EX, html.ViewData[StringConstant.APP_VD_TITEL]));
+                }
+            }
+            return new MvcHtmlString(string.Format(StringConstant.APP_NAME));
+        }
+
+        public static MvcHtmlString AppTitle(this HtmlHelper html, string title = StringConstant.EMPTY)
+        {
+            html.ViewData[StringConstant.APP_VD_TITEL] = title;
+            return new MvcHtmlString(String.Empty); 
+        }
+
+        public static MvcHtmlString GravatarUrl(this HtmlHelper html, 
+                                                string mail = StringConstant.EMPTY, 
+                                                string size = StringConstant.GRAVATAR_SIZE)
+        {            
+            return new MvcHtmlString(ImageSrc(mail, size));
+        }
+
+        public static MvcHtmlString Year(this HtmlHelper html)
+        {
+            return new MvcHtmlString(DateTime.Now.Year.ToString());
+        }
+
+        public static MvcHtmlString GravatarFor<TModel, TProperty>( this HtmlHelper<TModel> helper, 
+                                                                    Expression<Func<TModel, TProperty>> expression, 
+                                                                    object htmlAttributes,
+                                                                    string size = StringConstant.GRAVATAR_SIZE)
+        {
+            var valueGetter = expression.Compile();
+            var value = valueGetter(helper.ViewData.Model);
+            var mail = ImageSrc(value.ToString(), size);
+
+            var img = new TagBuilder(StringConstant.HTML_TAG_IMG);
+            img.MergeAttribute(StringConstant.HTML_ATTRI_IMG_SRC, mail);
+            img.MergeAttribute(StringConstant.HTML_ATTRI_IMG_ALT, String.Empty);            
+
+            img.MergeAttributes(new RouteValueDictionary(htmlAttributes));
+            img.SetInnerText(value.ToString());
+
+            return MvcHtmlString.Create(img.ToString(TagRenderMode.SelfClosing));
+        }
+
+        private static string ImageSrc(string email = StringConstant.EMPTY, 
+                                       string size = StringConstant.GRAVATAR_SIZE)
+        {
+            var md5 = MD5.Create();
+            var md5String = BitConverter.ToString(md5.ComputeHash(Encoding.UTF8.GetBytes(email.ToLower())))
+                                        .ToLower().Replace(StringConstant.GRAVATAR_MD5_RE, String.Empty);
+            md5.Dispose();
+            return string.Format(StringConstant.GRAVATAR_LINK , md5String, size);
+        }
+    }
+}
